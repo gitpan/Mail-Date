224bb96403f13e288c3b4d2436d9fa36f96079c3
畑正憲 <hata@iname.com>
2003-04-06 08:04:38 +0000

    Import of HATA/Mail-Date-0.07 from CPAN.
    gitpan-cpan-distribution: Mail-Date
    gitpan-cpan-version:      0.07
    gitpan-cpan-path:         HATA/Mail-Date-0.07.tar.gz
    gitpan-cpan-author:       HATA
    gitpan-cpan-maturity:     released

--- Date.pm
+++ Date.pm
@@ -3,39 +3,23 @@ package Mail::Date;
 use 5.008;
 use strict;
 use warnings;
-
-our $VERSION = '0.06_01'; # 2003-03-12
-
 use Carp;
 
-sub rfc2822 {
-	my ($class, $universal_time, $timezone) = @_;
-	my $self = $class->new();
-	$self->convert($universal_time, $timezone);
-	return $self->output();
-}
+our $VERSION = '0.07'; # 2003-04-06 (since 1999)
 
-sub new {
-	my $class = shift;
-	my $self = {};
-	bless $self, $class;
-	return $self;
-}
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT = qw(datetime_rfc2822);
 
-sub output {
-	my $self = shift;
-	return $$self{'date_time'};
-}
-
-sub convert {
-	my ($self, $universal_time, $timezone) = @_;
+sub datetime_rfc2822 {
+	my($universal_time, $timezone) = @_;
 	
 	unless ($timezone) {
 		$timezone = '-0000';
     }
 	
 	unless ($timezone =~ /^[\+\-]\d{4}$/) {
-		croak 'Invalid timezone expression: it must be in the format like +hhmm or -hhmm';
+		croak 'Invalid timezone expression: it must be in a format like +hhmm or -hhmm';
 	}
 	my $plus_minus    = substr($timezone, 0, 1);
 	my $timezone_hour = substr($timezone, 1, 2);
@@ -47,20 +31,18 @@ sub convert {
 		($timezone_hour * 3600 + $timezone_min * 60);
 	
 	my $local_standard_time = $universal_time + $timezone_sec;
-	my ($sec, $min, $hour, $mday, $mon, $year, $wday, undef, undef) =
+	my($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) =
 		gmtime($local_standard_time);
 	
-	$year = $year + 1900;
+	$year +=  1900;
 	$mon  = qw(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec)[$mon];
 	$wday = qw(Sun Mon Tue Wed Thu Fri Sat)[$wday];
 	
 	foreach my $digit ($mday, $hour, $min, $sec) {
-		$digit = sprintf("%02d", $digit);
+		$digit = sprintf('%02d', $digit);
 	}
 	
-	$$self{'date_time'} = "$wday, $mday $mon $year $hour:$min:$sec $timezone";
-	
-	return $self;
+	return "$wday, $mday $mon $year $hour:$min:$sec $timezone";
 }
 
 1;
@@ -68,46 +50,40 @@ __END__
 
 =head1 NAME
 
-Mail::Date - generates RFC2822 compliant date-time
+Mail::Date - convert to RFC2822 compliant date-time
 
 =head1 SYNOPSIS
 
   use Mail::Date;
-  print Mail::Date->rfc2822(time, '+0900');
+  print datetime_rfc2822(time, '+0900');
 
 =head1 DESCRIPTION
 
-Here it is rfc2822 compliant email date-time string generator. The well-known RFC822 has already been obsoleted by RFC2822 on April 2001. Then the standard format of date-time expression has been changed in RFC2822. You should not use such an old format like:
+Here it is a rfc2822 compliant date-time converter. The well-known RFC822 has already been obsoleted by RFC2822 on April 2001. And the standard format of expression for date-time has been updated in the RFC2822. An example which is shown next is obsoleted one:
 
  Thu, 06 Mar 2003 19:14:05 GMT
 
-any more. Although it is RFC822 compliant but RFC2822 compliant. Instead of that, you should use the local time in such a format +hhmm or -hhmm like:
+Because it has an alphabetical string C<GMT> for indicating a timezone. Now the RFC2822 specifies a timezone should be expressed in a numerical string like C<+hhmm> or C<-hhmm>. The former example should be displayed as next:
+
+ Thu, 06 Mar 2003 19:14:05 -0000
+
+Moreover, the RFC2822 describes timezone SHOULD express local-time. The timezone C<-0000> does not mean the local-time of GMT (Greenwich Mean Time) but does universal-time (UTC). If it mean local-time at a place of GMT, the timezone C<+0000> SHOULD be used. So you know where you are and the local timezone's offset from UTC, you would specify timezone and local-time. For example, when you were at the time of second example of UTC, and you were in Japanese local-time where it is ahead of nine hours from UTC, the date-time expression is:
 
  Fri, 07 Mar 2003 04:14:05 +0900
 
-Please consult RFC2822 (section 3.3, 4.3) for the futher infomation.
+(Don't overlook that it is not only timezone but the date and time-of-day are also altered.)
 
-=head1 METHODS
+Please consult RFC2822 (section 3.3, 4.3) for the actual infomation.
+
+=head1 FUNCTIONS
 
 =over
 
-=item rfc2822($machine_time [, $timezone])
+=item datetime_rfc2822($machine_time [, $timezone])
 
-Returns RFC2822 compliant date-time string which is converted from machine time. Though $timezone value is optional, it is said that in the RFC2822 `B<The date and time-of-day SHOULD express local time>'. If $timezone value is not given, it will be not taken as a local time but as a Universal Time (UTC).
+This function returns RFC2822 compliant date-time string which is converted from a unix-machine-time. Though $timezone value is optional, it is said that in the RFC2822 I<The date and time-of-day SHOULD express local time>. If $timezone value is not given (omitted), it will be not taken as a local-time but as a universal-time (UTC) to be expressed C<-0000>.
 
 The time zone expression should be compliant to the RFC2822 specification. It must be within the range -9959 through +9959. The "+" or "-" indicates whether the time-of-day is ahead of (i.e., east of) or behind (i.e., west of) Universal Time. The first two digits indicate the number of hours difference from Universal Time, and the last two digits indicate the number of minutes difference from Universal Time. (Hence, +hhmm means +(hh * 60 + mm) minutes, and -hhmm means -(hh * 60 + mm) minutes). The form "+0000" should be used to indicate a time zone at Universal Time.
-
-=item new()
-
-Just creates a new object.
-
-=item convert($machine_time, $timezone)
-
-Just converts from machine time to date-time string.
-
-=item output()
-
-Just output the converted date-time string.
 
 =back
 
@@ -117,7 +93,7 @@ Just output the converted date-time string.
 
 =item RFC2822: L<http://www.ietf.org/rfc/rfc2822.txt>
 
-=item L<HTTP::Date> (RFC822 compliant date-time)
+=item L<HTTP::Date> (for the RFC822 compliant date-time)
 
 =back
 
@@ -132,3 +108,4 @@ Copyright (c) 1999-2003 Masanori HATA. All rights reserved.
 This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
 
 =cut
+
--- t/1.t
+++ t/1.t
@@ -1,22 +1,41 @@
 # Before `make install' is performed this script should be runnable with
 # `make test'. After `make install' it should work as `perl 1.t'
 
-use Test::More tests => 6;
+use Test::More tests => 7;
 
 BEGIN { use_ok('Mail::Date') };
 
-my $date = Mail::Date->new();
+my $Time = 1047278957;
 
-isa_ok( $date, 'Mail::Date' );
-is( $date->convert(1047278957, '+0900')->output(),
-	'Mon, 10 Mar 2003 15:49:17 +0900',
-	'convert() then output()' );
-is( Mail::Date->rfc2822(1047278958, '+0900'),
-	'Mon, 10 Mar 2003 15:49:18 +0900',
-	'rfc2822() for the prepared time' );
-is( Mail::Date->rfc2822(1047278958),
-	'Mon, 10 Mar 2003 06:49:18 -0000',
-	'rfc2822() for the prepared time; omit timezone' );
-like( Mail::Date->rfc2822(time, '+0900'),
-	'/[A-Z][a-z][a-z], \d\d [A-Z][a-z][a-z] \d\d\d\d \d\d:\d\d:\d\d [\+\-]\d\d\d\d/',
-	'rfc2822() for the present time' );
+my $date = datetime_rfc2822($Time, '+0000');
+is( $date, 'Mon, 10 Mar 2003 06:49:17 +0000',
+	'for the prepared time: +0000 (GMT)' );
+
+$date = datetime_rfc2822($Time, '+0927');
+is( $date, 'Mon, 10 Mar 2003 16:16:17 +0927',
+	'for the prepared time: +0927' );
+
+$date = datetime_rfc2822($Time, '-0927');
+is( $date, 'Sun, 09 Mar 2003 21:22:17 -0927',
+	'for the prepared time: -0927' );
+
+$date = datetime_rfc2822($Time, '-0000');
+is( $date, 'Mon, 10 Mar 2003 06:49:17 -0000',
+	'for the prepared time: Universal Time' );
+
+$date = datetime_rfc2822($Time);
+is( $date, 'Mon, 10 Mar 2003 06:49:17 -0000',
+	'for the prepared time: omit timezone (Universal Time)' );
+
+$date = datetime_rfc2822($Time, '+0927');
+my $Wday = '(Sun|Mon|Tue|Wed|Thu|Fri|Sat)';
+my $Mday =
+	'(01|02|03|04|05|06|07|08|09|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31)';
+my $Mon = '(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)';
+my $Hour =
+	'(00|01|02|03|04|05|06|07|08|09|10|11|12|13|14|15|16|17|18|19|20|21|22|23)';
+my $Sec =
+	'(00|01|02|03|04|05|06|07|08|09|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|36|37|38|39|40|41|42|43|44|45|46|47|48|49|50|51|52|53|54|55|56|57|58|59|60)';
+like( $date,
+	qr/$Wday, $Mday $Mon \d{4} $Hour:[0-5]\d:$Sec [\+\-]\d\d[0-5]\d/o,
+	'for the current time: matching to regexp' );
